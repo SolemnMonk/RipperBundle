6dec7f416eed4a3d9cf97ec97300044066f40880
Solemn Monk <solemn.monk@gmail.com>
2018-12-05 07:47:08 -0600

    Added support for base commands
    * Implementation added for --add-credential
    * Implementation added for -l and --list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os, os.path, importlib, argparse, sys, json
-#operations to add: install module, install dependency, list aliases, resolve conflicts, add credentials
+import os, os.path, importlib, argparse, sys, json, getpass
 
 credentials = {}
 
@@ -7,23 +6,23 @@ def cd():
 	script_dir = os.path.abspath(os.path.dirname(__file__))
 	os.chdir(script_dir)
 
-def get_module_manifest(module_dir):
-	module = importlib.import_module(module_dir + ".manifest")
-	return getattr(module, "Manifest")
+def get_plugin_manifest(plugin_dir):
+	plugin = importlib.import_module(plugin_dir + ".manifest")
+	return getattr(plugin, "Manifest")
 
-def get_module_runner(module_dir):
-	module = importlib.import_module(module_dir + ".runner")
-	return getattr(module, "Runner")
+def get_plugin_runner(plugin_dir):
+	plugin = importlib.import_module(plugin_dir + ".runner")
+	return getattr(plugin, "Runner")
 
-def get_potential_modules():
+def get_potential_plugins():
 	files = os.listdir(".")
 	
-	potential_modules = []
+	potential_plugins = []
 	for file in files:
 		if os.path.isdir(file):
-			potential_modules.append(file)
+			potential_plugins.append(file)
 	
-	return potential_modules
+	return potential_plugins
 
 def is_valid_manifest(manifest):
 	valid = True
@@ -47,9 +46,9 @@ def is_valid_runner(runner):
 	
 	return valid
 
-def get_module_dirs(potential_modules):
-	modules = []
-	for pm in potential_modules:
+def get_plugin_dirs(potential_plugins):
+	plugins = []
+	for pm in potential_plugins:
 		contents = os.listdir("./" + pm)
 		if "__init__.py" not in contents:
 			continue
@@ -58,31 +57,31 @@ def get_module_dirs(potential_modules):
 		if "runner.py" not in contents:
 			continue
 		
-		manifest = get_module_manifest(pm)
+		manifest = get_plugin_manifest(pm)
 		if not is_valid_manifest(manifest):
 			continue
 		
-		runner = get_module_runner(pm)
+		runner = get_plugin_runner(pm)
 		if not is_valid_runner(runner):
 			continue
 		
-		modules.append(pm)
-	return modules
+		plugins.append(pm)
+	return plugins
 
-def get_aliases(module_dirs):
+def get_aliases(plugin_dirs):
 	aliases = {}
 	conflicts = {}
-	for module_dir in module_dirs:
-		manifest = get_module_manifest(module_dir)
+	for plugin_dir in plugin_dirs:
+		manifest = get_plugin_manifest(plugin_dir)
 		for alias in set(manifest.alias):
 			if alias in aliases.keys():
 				cs = conflicts.get("alias", [])
 				if len(cs) is 0:
 					cs.append(aliases[alias])
-				cs.append(module_dir)
+				cs.append(plugin_dir)
 				conflicts[alias] = cs
 				continue
-			aliases[alias] = module_dir
+			aliases[alias] = plugin_dir
 	return aliases, conflicts
 
 def print_alias_conflicts(conflicts):
@@ -94,50 +93,165 @@ def print_alias_conflicts(conflicts):
 def build_base_parser():
 	parser = argparse.ArgumentParser()
 	
-	parser.add_argument("-l", "--list", help="List the aliases and friendly names for all installed modules", action="store_true", dest="list")
-	parser.add_argument("--install-module", help="Install a new module; accepts a path to a directory", dest="install_module")
-	parser.add_argument("--install-dependency", help="Install a dependency for a module; accepts a path to a directory or a single file", dest="install_dependency")
-	parser.add_argument("--resolve-conflict", help="Resolve all alias conflicts; enters an interactive mode", action="store_true", dest="resolve_conflict")
-	parser.add_argument("--add-credentials", help="Add credentials for one or more plugins; enters an interactive mode", action="store_true", dest="credentials")
+	parser.add_argument("-l", "--list", help="List the aliases and friendly names for all installed plugin", action="store_true", dest="list")
+	parser.add_argument("--add-credentials", help="Interactive; add credentials for one or more plugins", action="store_true", dest="add_credentials")
+	parser.add_argument("--resolve-conflict", help="Interactive; resolve all alias conflicts", action="store_true", dest="resolve_conflict")
+	parser.add_argument("--install-plugin", help="Install a new plugin; accepts a path to a directory or the following archives: *.7z, *.zip, *.rar, *.tar, *.tar.gz", default=None, dest="install_plugin")
+	parser.add_argument("--install-dependency", help="Install a dependency for a plugin; if a path delimiter / or \\ is present, this accepts a path to a directory or a single file, otherwise it will attempt to install a Python package through pip", default=None, dest="install_dependency")
 
 	return parser
 
 def build_parsers(aliases):
 	parser = build_base_parser()
-	subparsers = parser.add_subparsers(dest="target_alias", metavar="pluginAlias")
+	subparsers = parser.add_subparsers(dest="target_alias", metavar="plugin_alias")
 
 	for alias in aliases:
-		manifest = get_module_manifest(aliases[alias])
+		manifest = get_plugin_manifest(aliases[alias])
 		sub_parser = subparsers.add_parser(alias)
 		manifest.get_parser(sub_parser)
 	
 	return parser
 
-def dispatch(target_module_dir, args):
+def dispatch(target_plugin_dir, args):
 	print("args: " + str(args))
-	manifest = get_module_manifest(target_module_dir)
+	manifest = get_plugin_manifest(target_plugin_dir)
 	credential_key = manifest.credential_key
 	
-	runner = get_module_runner(target_module_dir)
+	runner = get_plugin_runner(target_plugin_dir)
 	runner.run(args, credentials.get(credential_key))
 
 def load_credentials():
 	credentials_path = "credentials.json"
-	if os.path.exists(credentials_path):
-		global credentials
-		
-		with open(credentials_path) as f:
-			credentials = json.loads(f.read())
+	if not os.path.exists(credentials_path):
+		c = open("credentials.json", "w")
+		c.write("{}")
+		c.close()
+	global credentials
+	
+	with open(credentials_path) as f:
+		credentials = json.loads(f.read())
+
+def should_dispatch_to_plugin(args):
+	dispatch = True
+	
+	dispatch = dispatch and not args.list
+	dispatch = dispatch and not args.add_credentials
+	dispatch = dispatch and not args.resolve_conflict
+	dispatch = dispatch and not args.install_plugin is None
+	dispatch = dispatch and not args.install_dependency is None
+
+	return dispatch
+
+def list_aliases(aliases):
+	sorted_aliases = {}
+	for alias in aliases:
+		if aliases[alias] not in sorted_aliases:
+			sorted_aliases[aliases[alias]] = []
+		sorted_aliases[aliases[alias]].append(alias)
+
+	print()
+	print("The following plugins are installed; their aliases are listed after them:")
+	for alias in sorted_aliases:
+		print("\t" + alias + " : " + ", ".join(sorted_aliases[alias]))
+
+def add_credentials(aliases):
+	print()
+	print("Which plugin would you like to add credentials for?")
+	
+	selection = -1
+	plugin_dirs = sorted(list(set(aliases.values())))
+	while True:
+		try:
+			i = 1
+
+			print("0: <quit application>")
+			for plugin_dir in plugin_dirs:
+				print(str(i) + ": " + plugin_dir)
+			
+			selection = input("> ").strip()
+			selection = int(selection)
+			if selection < 0 or selection > len(plugin_dirs):
+				raise ValueError("Invalid selection")
+			break
+		except:
+			print("Invalid selection, please choose a number between 1 and " + str(len(plugin_dirs)))
+
+	if selection is 0:
+		print("Exiting...")
+		return
+	
+	manifest = get_plugin_manifest(plugin_dirs[selection - 1])
+	credential_key = manifest.credential_key
+
+	print("Enter the username to add:")
+	username = input("> ")
+	print("Enter the password:")
+	password = getpass.getpass("> ")
+
+	additional_fields = {}
+	print("If there are any additional fields you need to add to this credential, you may add them now.")
+	while True:
+		print("What is the field's name? Enter to skip")
+		field_name = input("> ")
+		if (field_name == ""):
+			break
+		print("What is the value for '" + field_name + "'?")
+		field_value = input("> ")
+		additional_fields[field_name] = field_value
+
+	credential = {"username" : username, "password" : password}
+	for field in additional_fields:
+		credential[field] = additional_fields[field]
+
+	load_credentials()
+
+	global credentials
+	credentials[credential_key] = credential
+
+	credentials_path = "credentials.json"
+	with open(credentials_path, "w") as f:
+		f.write(json.dumps(credentials, indent=2, sort_keys=True))
+	
+	with open(credentials_path) as f:
+		loaded_credentials = json.loads(f.read())
+		print(credentials)
+		print(loaded_credentials)
+		if loaded_credentials == credentials:
+			print("Credential added; exiting...")
+		else:
+			print("Failed to add credential; exiting...")
+
+
+def resolve_conflict(aliases):
+	pass
+
+def install_plugin(args):
+	pass
+
+def install_dependency(args):
+	pass
+
+def handle_core_arguments(args, aliases):
+	if args.list:
+		list_aliases(aliases)
+	elif args.add_credentials:
+		add_credentials(aliases)
+	elif args.resolve_conflict:
+		resolve_conflict(aliases)
+	elif args.install_plugin is not None:
+		install_plugin(args)
+	elif args.install_dependency is not None:
+		install_dependency(args)
 
 def main():
 	cd()
 
 	load_credentials()
 
-	potential_modules = get_potential_modules()
-	module_dirs = get_module_dirs(potential_modules)
+	potential_plugins = get_potential_plugins()
+	plugin_dirs = get_plugin_dirs(potential_plugins)
 
-	aliases, conflicts = get_aliases(module_dirs)
+	aliases, conflicts = get_aliases(plugin_dirs)
 
 	if len(conflicts) > 0:
 		print_alias_conflicts(conflicts)
@@ -148,7 +262,10 @@ def main():
 
 	args = parsers.parse_args()
 
-	dispatch(aliases[args.target_alias], args)
-	
+	if (should_dispatch_to_plugin(args)):
+		dispatch(aliases[args.target_alias], args)
+	else:
+		handle_core_arguments(args, aliases)
+
 if __name__ == "__main__":
 	main()
