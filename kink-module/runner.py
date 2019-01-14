89efde04bc1f297b919307e6e1242f745f3d9e38
Solemn Monk <solemn.monk@gmail.com>
2019-01-14 08:16:11 -0600

    Snapshot

--- kink-module/runner.py
+++ kink-module/runner.py
@@ -54,7 +54,7 @@ class Kink:
 	driver = None
 	photo_dispatcher_thread = None
 	trailer_dispatcher_thread = None
-	video_dispatcher_thread = None
+	movie_dispatcher_thread = None
 	clip_dispatcher_thread = None
 	stop_dispatching = False
 	
@@ -64,6 +64,9 @@ class Kink:
 	#	download_command is a tuple
 	#	unzip_command is a tuple
 	photo_argument_lists = []
+	clip_argument_lists = []
+	movie_argument_lists = []
+	trailer_argument_lists = []
 	
 	sites = None
 
@@ -74,6 +77,12 @@ class Kink:
 		
 		Kink.photo_dispatcher_thread = threading.Thread(
 			target=self.__dispatch_photos)
+		Kink.clip_dispatcher_thread = threading.Thread(
+			target=self.__dispatch_clips)
+		Kink.photo_dispatcher_thread = threading.Thread(
+			target=self.__dispatch_movie)
+		Kink.clip_dispatcher_thread = threading.Thread(
+			target=self.__dispatch_trailer)
 		
 		self.username = username
 		self.password = password
@@ -102,83 +111,6 @@ class Kink:
 		self.__load_sites()
 
 		Kink.photo_dispatcher_thread.start()
-		
-	def __load_sites(self):
-		sites_path = os.path.join(".", "kink-module", "sites.json")
-		if os.path.exists(sites_path):
-			with open(sites_path) as f:
-				self.sites = json.loads(f.read())
-
-	def __dispatch_photos(self):
-		print("Beginning photo dispatcher thread...")
-		while not Kink.stop_dispatching or len(Kink.photo_argument_lists) != 0:
-			if len(Kink.photo_argument_lists) == 0:
-				continue
-
-			print("Argument list found! Dispatching...")
-			command_list = Kink.photo_argument_lists.pop(0)
-			id = command_list[0]
-			command_list = command_list[1:]
-			aria_command_list = [c[0] for c in command_list]
-			_7z_command_list = [c[1] for c in command_list]
-
-			pool = multiprocessing.Pool(self.process_limit)
-			
-			pool.map(self.download_media, aria_command_list)
-			pool.map(self.unzip_media, _7z_command_list)
-
-			zip_paths = [
-				os.path.join(c[1][2], c[1][4]) for c in aria_command_list]
-			
-			for z in zip_paths:
-				os.remove(z)
-
-			photo_dir = aria_command_list[0][1][2]
-
-			photo_rename_command = self.build_photo_rename_command(
-				photo_dir, id)
-			subprocess.run(photo_rename_command)
-
-			self.suction_photos(photo_dir)
-				
-		print("Exiting photo dispatcher thread...")
-
-	def suction_photos(self, dir, is_prime_invocation=True):
-		dirs = []
-		files = []
-
-		contents = os.scandir(dir)
-		for i in contents:
-			if i.is_dir():
-				dirs.append(os.path.join(dir, i.name))
-			else:
-				files.append(os.path.join(dir, i.name))
-
-		if len(dirs) > 0:
-			for d in dirs:
-				files += self.suction_photos(d, False)
-		
-		if is_prime_invocation:
-			for f in files:
-				shutil.move(f, dir)
-			for d in dirs:
-				shutil.rmtree(d)
-			parent = os.path.split(dir)[0]
-			os.rename(dir, os.path.join(parent, "Photos"))
-		else:
-			return files
-
-	def __build_channel_url(self, name):
-		return "https://www.kink.com/channel/" + name
-
-	def __build_performer_url(self, id):
-		return "https://www.kink.com/model/" + id
-	
-	def __build_shoot_url(self, url):
-		if "kink.com" in url:
-			return url
-		else:
-			return "https://www.kink.com/shoot/" + id
 
 	def startup(self):
 		Kink.driver = webdriver.Chrome(
@@ -192,41 +124,6 @@ class Kink:
 	def shutdown(self):
 		Kink.driver.quit()
 	
-	def __bypass_splash_screen(self):
-		splash_screen_xpath = "//form[@id='contentTypeModal']"
-		show_everything_xpath = "//button[@type='submit' and contains(@value,'gay') and contains(@value,'straight')]"
-
-		try:
-			Kink.driver.find_element_by_xpath(splash_screen_xpath)
-			Kink.driver.find_element_by_xpath(show_everything_xpath).click()
-		except:
-			print("No splash screen to bypass")
-			pass
-
-	def __login(self):
-		login_button_xpath = "//a[@id='kBarLogin']"
-		login_form_submit_xpath = "//button[@type='submit' and @name='login']"
-		username_box_xpath = "//input[@name='username']"
-		password_box_xpath = "//input[@name='password']"
-		
-		Kink.driver.find_element_by_xpath(login_button_xpath).click()
-		time.sleep(1)
-		Kink.driver.find_element_by_xpath(username_box_xpath).send_keys(
-			self.username)
-		Kink.driver.find_element_by_xpath(password_box_xpath).send_keys(
-			self.password)
-		Kink.driver.find_element_by_xpath(login_form_submit_xpath).click()
-
-		time.sleep(5)
-		
-		while True:
-			try:
-				# Figure out what's needed for detecting captchas
-				break
-			except:
-				break
-		print("No captcha found!")
-
 	def rip(self):
 		for url in self.urls:
 			Kink.driver.get(url)
@@ -247,7 +144,7 @@ class Kink:
 		print("Total shoots ripped: " + str(self.shoots_completed))
 		print("Total channels ripped: " + str(self.channels_completed))
 		print("Total performers ripped: " + str(self.performers_completed))
-
+		
 	def __rip_channel(self):
 		print("Sorry bro, not done yet.")
 
@@ -285,21 +182,138 @@ class Kink:
 		if self.photos:
 			self.__save_photos(site, title)
 
-		# if self.clips:
-		# 	self.__save_clips()
+		if self.clips:
+			self.__save_clips()
 		# else:
 		# 	self.__save_video()
 		
 		self.shoots_completed += 1
 
+	def __load_sites(self):
+		sites_path = os.path.join(".", "kink-module", "sites.json")
+		if os.path.exists(sites_path):
+			with open(sites_path) as f:
+				self.sites = json.loads(f.read())
+
+	def __dispatch_photos(self):
+		print("Beginning photo dispatcher thread...")
+		while not Kink.stop_dispatching or len(Kink.photo_argument_lists) != 0:
+			if len(Kink.photo_argument_lists) == 0:
+				continue
+
+			print("Argument list found! Dispatching...")
+			command_list = Kink.photo_argument_lists.pop(0)
+			id = command_list[0]
+			command_list = command_list[1:]
+			aria_command_list = [c[0] for c in command_list]
+			_7z_command_list = [c[1] for c in command_list]
+
+			pool = multiprocessing.Pool(self.process_limit)
+			
+			pool.map(self.__download_media, aria_command_list)
+			pool.map(self.__unzip_media, _7z_command_list)
+
+			zip_paths = [
+				os.path.join(c[1][2], c[1][4]) for c in aria_command_list]
+			
+			for z in zip_paths:
+				os.remove(z)
+
+			photo_dir = aria_command_list[0][1][2]
+
+			photo_rename_command = self.__build_photo_rename_command(
+				photo_dir, id)
+			subprocess.run(photo_rename_command)
+
+			self.__suction_photos(photo_dir)
+				
+		print("Exiting photo dispatcher thread...")
+
+	def __dispatch_clips(self):
+		print("Beginning clip dispatcher thread...")
+		while not Kink.stop_dispatching or len(Kink.clip_argument_lists) != 0:
+			if len(Kink.clip_argument_lists) == 0:
+				continue
+
+			print("Argument list found! Dispatching...")
+				
+		print("Exiting clip dispatcher thread...")
+
+	def __dispatch_movie(self):
+		print("Beginning movie dispatcher thread...")
+		while not Kink.stop_dispatching or len(Kink.movie_argument_lists) != 0:
+			if len(Kink.movie_argument_lists) == 0:
+				continue
+
+			print("Argument list found! Dispatching...")
+				
+		print("Exiting clip dispatcher thread...")
+
+	def __dispatch_trailer(self):
+		print("Beginning trailer dispatcher thread...")
+		while not Kink.stop_dispatching or len(Kink.trailer_argument_lists) != 0:
+			if len(Kink.trailer_argument_lists) == 0:
+				continue
+
+			print("Argument list found! Dispatching...")
+				
+		print("Exiting clip dispatcher thread...")
+
+	def __build_channel_url(self, name):
+		return "https://www.kink.com/channel/" + name
+
+	def __build_performer_url(self, id):
+		return "https://www.kink.com/model/" + id
+	
+	def __build_shoot_url(self, url):
+		if "kink.com" in url:
+			return url
+		else:
+			return "https://www.kink.com/shoot/" + id
+	
+	def __bypass_splash_screen(self):
+		splash_screen_xpath = "//form[@id='contentTypeModal']"
+		show_everything_xpath = "//button[@type='submit' and contains(@value,'gay') and contains(@value,'straight')]"
+
+		try:
+			Kink.driver.find_element_by_xpath(splash_screen_xpath)
+			Kink.driver.find_element_by_xpath(show_everything_xpath).click()
+		except:
+			print("No splash screen to bypass")
+			pass
+
+	def __login(self):
+		login_button_xpath = "//a[@id='kBarLogin']"
+		login_form_submit_xpath = "//button[@type='submit' and @name='login']"
+		username_box_xpath = "//input[@name='username']"
+		password_box_xpath = "//input[@name='password']"
+		
+		Kink.driver.find_element_by_xpath(login_button_xpath).click()
+		time.sleep(1)
+		Kink.driver.find_element_by_xpath(username_box_xpath).send_keys(
+			self.username)
+		Kink.driver.find_element_by_xpath(password_box_xpath).send_keys(
+			self.password)
+		Kink.driver.find_element_by_xpath(login_form_submit_xpath).click()
+
+		time.sleep(5)
+		
+		while True:
+			try:
+				# Figure out what's needed for detecting captchas
+				break
+			except:
+				break
+		print("No captcha found!")
+	
 	#(error_strings, command, str(i + 1), url, title, False)
-	def download_media(self, args):
+	def __download_media(self, args):
 		process = subprocess.run(args[1])
 		if process.returncode != 0:
 			args[0].append("Download failed; command: " + args[1])
 
 	#(error_strings, command, str(i + 1), url, title, False)
-	def unzip_media(self, args):
+	def __unzip_media(self, args):
 		process = subprocess.run(args[1])
 		if process.returncode != 0:
 			args[0].append("Unzip failed; command: " + str(args[1]))
@@ -325,16 +339,32 @@ class Kink:
 
 		args = []
 		for i in range(0, len(zips)):
-			aria_command_handle = self.build_photo_zip_aria_command(
+			aria_command_handle = self.__build_photo_zip_aria_command(
 				zips, i, dir_name, error_strings, site, title)
-			_7z_command_handle = self.build_photo_zip_7z_command(
+			_7z_command_handle = self.__build_photo_zip_7z_command(
 				zips, i, dir_name, error_strings, site, title)
 			
 			args.append([aria_command_handle, _7z_command_handle])
 
 		Kink.photo_argument_lists.append([id] + args)
 
-	def build_photo_zip_aria_command(self, zips, i, dir_name, error_strings, 
+	def __save_trailer(self):
+		trailer_button_xpath = "//div[@class='trailer']"
+		print("Pending evaluation")
+
+	def __save_clips(self):
+		clips_button_xpath = "//div[@class='clips']"
+		clips_categories_xpath = "//ul[contains(@class,'clips') and contains(@class,'open')]/li"
+		clips_xpath = "//ul[contains(@class,'clips') and contains(@class,'open')]/li/ul/li/a"
+		print("Sorry bro, not done yet.")
+
+	def __save_video(self):
+		movie_button_xpath = "//div[@class='full']"
+		movie_quality_xpath = "//ul[contains(@class,'full-movie') and contains(@class,'open')]"
+		movie_xpath = "//ul[contains(@class,'full-movie') and contains(@class,'open')]/li/a"
+		print("Sorry bro, not done yet.")
+	
+	def __build_photo_zip_aria_command(self, zips, i, dir_name, error_strings, 
 		site, title):
 		
 		aria_path = os.path.join("3rd-party-tools", "aria2", "aria2c.exe")
@@ -352,7 +382,7 @@ class Kink:
 		command_handle = (error_strings, command)
 		return command_handle
 
-	def build_photo_zip_7z_command(self, zips, i, dir_name, error_strings, 
+	def __build_photo_zip_7z_command(self, zips, i, dir_name, error_strings, 
 		site, title):
 		
 		_7z_path = os.path.join("3rd-party-tools", "7z.exe")
@@ -370,31 +400,40 @@ class Kink:
 		command_handle = (error_strings, command)
 		return command_handle
 
-	def build_photo_rename_command(self, dir, id):
+	def __build_photo_rename_command(self, dir, id):
 		brc_path = os.path.join("3rd-party-tools", "BRC64.exe")
 		command = [brc_path, "/NOFOLDERS", "/DIR:" + dir, "/RECURSIVE", 
 			f"/FIXEDNAME:Shoot {id} Image ", "/AUTONUMBER:1:1:S::10:3", 
 			"/EXECUTE"]
 		return command
 
-	def __save_trailer(self):
-		trailer_button_xpath = "//div[@class='trailer']"
-		print("Pending evaluation")
-
-	def __save_clips(self):
-		clips_button_xpath = "//div[@class='clips']"
-		clips_categories_xpath = "//ul[contains(@class,'clips') and contains(@class,'open')]/li"
-		clips_xpath = "//ul[contains(@class,'clips') and contains(@class,'open')]/li/ul/li/a"
-		print("Sorry bro, not done yet.")
-
 	def __join_clips(self):
 		pass
 
-	def __save_video(self):
-		movie_button_xpath = "//div[@class='full']"
-		movie_quality_xpath = "//ul[contains(@class,'full-movie') and contains(@class,'open')]"
-		movie_xpath = "//ul[contains(@class,'full-movie') and contains(@class,'open')]/li/a"
-		print("Sorry bro, not done yet.")
+	def __suction_photos(self, dir, is_prime_invocation=True):
+		dirs = []
+		files = []
+
+		contents = os.scandir(dir)
+		for i in contents:
+			if i.is_dir():
+				dirs.append(os.path.join(dir, i.name))
+			else:
+				files.append(os.path.join(dir, i.name))
+
+		if len(dirs) > 0:
+			for d in dirs:
+				files += self.__suction_photos(d, False)
+		
+		if is_prime_invocation:
+			for f in files:
+				shutil.move(f, dir)
+			for d in dirs:
+				shutil.rmtree(d)
+			parent = os.path.split(dir)[0]
+			os.rename(dir, os.path.join(parent, "Photos"))
+		else:
+			return files
 
 def print_warning():
 	print("This file is meant to be imported by other Python files, not run directly. Exiting now.")
